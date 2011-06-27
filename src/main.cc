f75fd4fc2fa6f8e5b68cd5d55124e375fc54d271
Dmitrij Smirnov <other@igus.lv>
2011-06-27 20:33:46 +0300

    some updates

--- src/main.cc
+++ src/main.cc
@@ -18,15 +18,17 @@ int main(int argc, char **argv)
         fclose(pFile);
 
 		if (argc>1) {
-			if(!strcmp(argv[1], "-d")) start_flag=1;
-			else if(!strcmp(argv[1], "-h")) start_flag=6;
+			//if(!strcmp(argv[1], "-d")) start_flag=1;
+			//else 
+			if(!strcmp(argv[1], "-h")) start_flag=6;
 			else if(!strcmp(argv[1], "-l")) start_flag=3;
 			else if(!strcmp(argv[1], "-p")) start_flag=4;
 			else if(!strcmp(argv[1], "-s")) start_flag=5;
 		}
 
-		if (start_flag==1) DowngradeToDate (argv[2]);
-		else if (start_flag==2) ShowStartWindow(actions_counter,&packages[0]);
+		//if (start_flag==1) DowngradeToDate (argv[2]);
+		//else 
+		if (start_flag==2) ShowStartWindow(actions_counter,&packages[0]);
 		else if (start_flag==3) DowngradeLastPackages (atoi(argv[2]), actions_counter, &packages[0]);
 		else if (start_flag==4) SilentDowngradePackage (argv[2], actions_counter,&packages[0]);
 		else if (start_flag==5) PacmanStatistic (actions_counter);
